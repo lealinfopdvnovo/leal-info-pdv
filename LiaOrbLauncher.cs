04925782eb6de1f430bd768ccf4c7a147a991aaf
lealinfopdvnovo <lealinfopdvnovo@gmail.com>
2026-09-06 13:47:33 -0300

    Add files via upload

--- LiaOrbLauncher.cs
+++ LiaOrbLauncher.cs
@@ -1,4 +1,4 @@
-using System.Drawing.Drawing2D;
+﻿using System.Drawing.Drawing2D;
 
 namespace LealInfoPDV;
 
@@ -13,6 +13,14 @@ public sealed class LiaOrbLauncher : Control
         Size = new Size(68, 68);
         Cursor = Cursors.Hand;
         TabStop = false;
+
+        // WinForms exige este estilo antes de aceitar Color.Transparent
+        // em um Control personalizado. Sem isso, o PDV falha na inicializacao.
+        SetStyle(ControlStyles.UserPaint |
+                 ControlStyles.AllPaintingInWmPaint |
+                 ControlStyles.OptimizedDoubleBuffer |
+                 ControlStyles.SupportsTransparentBackColor, true);
+        UpdateStyles();
         DoubleBuffered = true;
         BackColor = Color.Transparent;
         timer.Tick += (_, _) => { fase += 0.10; Invalidate(); };
