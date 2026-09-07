5b4144d7f11721f08435412029ee5efd904983b0
lealinfopdvnovo <lealinfopdvnovo@gmail.com>
2026-09-06 23:11:50 -0300

    Exibir versão V10.133 no PDV

--- MainForm.cs
+++ MainForm.cs
@@ -22,7 +22,7 @@ public sealed class MainForm : Form
 
     public MainForm()
     {
-        Text = "LEAL INFO CONECTADO - SISTEMA PDV - V10.130";
+        Text = "LEAL INFO CONECTADO - SISTEMA PDV - V10.133";
         WindowState = FormWindowState.Maximized;
         MinimumSize = new Size(1200, 720);
         BackColor = Color.White;
